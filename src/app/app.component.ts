3ffb9e9c834d2b6ff26e1ef0ebab80a62b6dbd45
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2022-01-15 20:32:35 +0530

    yt downloader setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'yt-thumbnail';
+  show = false;
+  url = "https://i.ytimg.com/vi/";
+  urlPost = "/maxresdefault.jpg";
+  finalUrl = "";
+  downloadImg(str) {
+    this.finalUrl = "";
+    this.finalUrl += this.url; 
+    this.finalUrl += str.split("=")[1];
+    this.finalUrl += this.urlPost;
+    this.show = true;
+    console.log(this.finalUrl)
+  }
+
 }
